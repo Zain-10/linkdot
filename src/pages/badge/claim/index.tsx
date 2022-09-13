f61c2303cb6566f9cc5ebd741ae263475033b79f
sreejin <sreejinsreenivasan93@gmao>
2022-09-13 13:07:46 +0530

    fix: add .env to .git ignore

--- src/pages/badge/claim/index.tsx
+++ src/pages/badge/claim/index.tsx
@@ -1,12 +1,16 @@
+import { useAddress } from "@thirdweb-dev/react";
+import type { GetServerSideProps, NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
 import { Connect } from "@/components/connect";
 import { apiRoutes } from "@/config/apiRoutes";
+import { LocalRoutes } from "@/config/localRoutes";
 import { Badges } from "@/fixtures/badges";
 import { axiosClient } from "@/helpers/axios-client";
 import { getIPFSGatewayURL } from "@/helpers/utils/ipfs";
 import { Base } from "@/layouts/Base";
-import { useAddress } from "@thirdweb-dev/react";
-import type { GetServerSideProps, NextPage } from "next";
-import { useEffect, useState } from "react";
+
 import { BadgeCard } from "../../../components/badge/Card";
 import { Badge } from "../../../components/badge/NTTBadge";
 
@@ -18,6 +22,7 @@ type PageProps = {
 const Claim: NextPage<PageProps> = ({ email_data, badge_data }) => {
   const [badge, setBadge] = useState<NTTBadge>();
   const address = useAddress();
+  const router = useRouter();
 
   const claimBadge = async () => {
     const payload = {
@@ -25,7 +30,10 @@ const Claim: NextPage<PageProps> = ({ email_data, badge_data }) => {
       badge_data: badge_data.split(" ").join("+"),
     };
     const response = await axiosClient.post(apiRoutes.claimBadge, payload);
-    console.log(response);
+    if (response.status === 200) {
+      alert("Badge claimed successfully");
+      router.push(LocalRoutes.dashboard);
+    }
   };
 
   useEffect(() => {
@@ -60,13 +68,16 @@ const Claim: NextPage<PageProps> = ({ email_data, badge_data }) => {
           </BadgeCard>
         </div>
         <div className="w-full md:w-2/6"></div>
-        <div className="flex w-full flex-col justify-between md:w-2/6">
-          <p className="mb-10 text-center text-sm">
-            This Badge is issued to sample@email.com. Please connect the wallet
-            to claim.
-          </p>
-          <Connect />
-        </div>
+        {!address && (
+          <div className="flex w-full flex-col justify-between md:w-2/6">
+            <p className="mb-10 text-center text-sm">
+              This Badge is issued to sample@email.com. Please connect the
+              wallet to claim.
+            </p>
+            <Connect />
+          </div>
+        )}
+
         <div className="md:w-1/6"></div>
       </div>
     </Base>
