b2eec59da86b0e27a8c7c7436863710ecd882059
sreejin <sreejinsreenivasan93@gmao>
2022-09-17 16:28:41 +0530

    fix: add-new-utils-function-to-return-redirect-pages

--- src/helpers/utils/getAuthRedirectPage.ts
+++ src/helpers/utils/getAuthRedirectPage.ts
@@ -7,4 +7,19 @@ function authRedirectPage(user: User): string | null {
   return null;
 }
 
-export { authRedirectPage };
+const fallbackToAuthPath = (user: User, callBackPath: string) => {
+  // TODO: needs to be refactored
+  // possible cases:
+  // 1. user category is not set
+  // 2. user name is not set
+  // 3. user email is not set
+  // 4. user is fully set up
+
+  if (user?.category && user?.user_name && user?.email) return callBackPath;
+  if (!user?.category) return LocalRoutes.authSelectCategory;
+  if (!user?.user_name) return LocalRoutes.authSetUserName;
+  if (!user?.email) return LocalRoutes.authSetEmail;
+  return LocalRoutes.dashboard;
+};
+
+export { authRedirectPage, fallbackToAuthPath };
