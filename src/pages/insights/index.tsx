eeae7f3964549347fd68d64793e94e9395cf24ea
sreejin <sreejinsreenivasan93@gmao>
2022-10-08 23:11:08 +0530

    feat: populate issued badges to table

--- src/pages/insights/index.tsx
+++ src/pages/insights/index.tsx
@@ -9,35 +9,57 @@ import { badgeService } from "@/helpers/service/badge";
 import { Main } from "@/layouts/Main/Main";
 
 const Insights: NextPage = () => {
-  const [badges, setBadges] = useState<NTTBadge[]>();
+  const [issuedBadges, setIssuedBadges] = useState<NTTBadge[]>();
+  const [claimedBadges, setIssuedClaimedBadges] = useState<NTTBadge[]>();
   const TabList: BadgeType[] = Object.values(BadgeType);
   const [activeTab, setActiveTab] = useState<BadgeType>(BadgeType.ISSUED);
-
+  console.log(claimedBadges);
   const onChange = (tabItem: BadgeType) => {
     setActiveTab(tabItem);
   };
   const fetchBadgesByType = async (badgeType: BadgeType) => {
     const response = await badgeService.getBadges(badgeType);
     if (response) {
-      const newBadges =
+      const badges =
         activeTab === BadgeType.ISSUED
           ? // @ts-ignore
             [...response.badges_issued]
           : // @ts-ignore
             [...response.badges_earned.map((e) => e.badge_id)];
-      setBadges(newBadges);
-      console.log(badges);
+      return badges;
     }
+    return [];
   };
-
+  const fetchIssuedBadges = async () => {
+    const badges = await fetchBadgesByType(BadgeType.ISSUED);
+    setIssuedBadges(badges);
+  };
+  const fetchClaimedBadges = async () => {
+    const badges = await fetchBadgesByType(BadgeType.CLAIMED);
+    setIssuedClaimedBadges(badges);
+  };
   useEffect(() => {
     fetchBadgesByType(activeTab);
   }, [activeTab]);
 
+  useEffect(() => {
+    fetchIssuedBadges();
+    fetchClaimedBadges();
+  }, []);
   return (
     <Main>
       <Tab tabList={TabList} onChangeCallBack={onChange} activeTab={activeTab}>
-        {activeTab === BadgeType.ISSUED ? <IssuedTable /> : <ClaimTable />}
+        {activeTab === BadgeType.ISSUED ? (
+          <>
+            {issuedBadges ? (
+              <IssuedTable badges={issuedBadges} />
+            ) : (
+              <div>No Data Available</div>
+            )}
+          </>
+        ) : (
+          <ClaimTable />
+        )}
       </Tab>
     </Main>
   );
