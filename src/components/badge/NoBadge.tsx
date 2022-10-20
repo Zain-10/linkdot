bbcac64a0295d89bba68e09cbc2b5299cb4be06b
sreejin <sreejinsreenivasan93@gmao>
2022-10-20 10:35:20 +0530

    Revert "fix: refactor badge list render logic"
    This reverts commit 9283e12fda57d1362c940c955cfd4dadbbcb3a0a.

--- src/components/badge/NoBadge.tsx
+++ src/components/badge/NoBadge.tsx
@@ -1,8 +1,29 @@
-interface BaseProps extends React.PropsWithChildren {}
+import Link from "next/link";
 
-const NoBadge = ({ children }: BaseProps) => (
+import { LocalRoutes } from "@/config/localRoutes";
+
+import { Button } from "../button";
+
+const NoBadge = () => (
   <div className="m-auto flex h-full items-center justify-center text-center md:w-1/2">
-    {children}
+    <div className="">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />
+        In urna et enim cursus purus laoreet mattis etiam.
+      </p>
+
+      <Link href={LocalRoutes.badge.create}>
+        <div className="mx-auto mt-6 w-1/2">
+          <Button
+            boxShadowVariant={2}
+            borderWidth={"2px"}
+            outerBoxShadowColor="#A58E09"
+          >
+            <span className="p-2 font-semibold">Create Badge</span>
+          </Button>
+        </div>
+      </Link>
+    </div>
   </div>
 );
 
