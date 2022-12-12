b8a0865a3cfc2cb8050fd47b9330bb9118a4dd79
sreejin <sreejinsreenivasan93@gmao>
2022-12-12 21:05:23 +0530

    feat: add api routes for user apis

---
+++ src/pages/api/users/[id]/follow.tsx
@@ -0,0 +1,7 @@
+import { followUser } from "@/backend/api/handler/user";
+import { MethodHandler } from "@/backend/helpers/method-handler";
+import { HttpMethod } from "@/constants";
+
+export default MethodHandler({
+  [HttpMethod.POST]: followUser,
+});
---
+++ src/pages/api/users/[id]/index.tsx
@@ -0,0 +1,7 @@
+import { getUser } from "@/backend/api/handler/user";
+import { MethodHandler } from "@/backend/helpers/method-handler";
+import { HttpMethod } from "@/constants";
+
+export default MethodHandler({
+  [HttpMethod.GET]: getUser,
+});
