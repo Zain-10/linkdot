5e1fd025b888e5c99a2ea9ae60afe0b1426af5c2
sreejin <sreejinsreenivasan93@gmao>
2022-12-12 20:55:05 +0530

    chore: chores

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -1,8 +1,16 @@
 type User = {
+  id: string;
+  name: string;
+  email: string;
+  walletId: string;
+  walletName: string;
+  following: string[];
+  followers: string[];
+
+  // TODO: To be removed
+  created_at: string;
   wallet_id: string;
   wallet_name: string;
   user_name: string;
   category: string;
-  email: string;
-  created_at: string;
 };
