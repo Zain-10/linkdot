35b2388cf4f08ef283fb096ea4a1212fc8b6177d
sreejin <sreejinsreenivasan93@gmao>
2023-01-27 15:21:51 +0530

    feat: configure codegen.yml

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,18 +4,24 @@ import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import type { AppProps } from "next/app";
 
 import GlobalProvider from "@/context/providers/global.provider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 /**
  * Configure Persistent Page Layouts
  * https://nextjs.org/docs/basic-features/layouts#per-page-layouts
  */
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <ThirdwebProvider desiredChainId={ChainId.Mainnet}>
-    <GlobalProvider>
-      <Component {...pageProps} />
-    </GlobalProvider>
-  </ThirdwebProvider>
-);
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const queryClient = new QueryClient();
+  return (
+    <ThirdwebProvider desiredChainId={ChainId.Mainnet}>
+      <QueryClientProvider client={queryClient}>
+        <GlobalProvider>
+          <Component {...pageProps} />
+        </GlobalProvider>
+      </QueryClientProvider>
+    </ThirdwebProvider>
+  );
+};
 
 export default MyApp;
