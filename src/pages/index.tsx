7e17f86c924eed1e76de4ed73a74fb2ac33e3e24
sreejin <sreejinsreenivasan93@gmao>
2023-01-29 11:11:06 +0530

    feat: add index page logic for fecthing top publications & user profiles from lens

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,79 @@
-import { useAddress } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
-import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
-import { LinkDotLoader } from "@/components/loader";
-import { Main } from "@/components/main";
-import { Profile } from "@/components/profile";
-import { useUserState } from "@/context/global.context";
+import { ConnectComponent } from "@/components/connect";
+import {
+  PublicationSortCriteria,
+  useExplorePublicationsQuery,
+  useProfilesQuery,
+} from "@/graphql/generated";
 
-const Home: NextPage = () => {
-  const [loading, setLoading] = useState(true);
-  const address = useAddress();
-  const router = useRouter();
-  const user = useUserState();
+const CHINDU_GREY_PROFILE_ID = "0x019bb6";
+const PUB_LIMIT = 5;
 
-  const stopLoading = () => setLoading(false);
+const Explore: NextPage = () => {
+  const [profileIds, setProfileIds] = useState<string[]>([]);
+
+  // get the list of publications
+  const { data: publicationsData, isLoading: isPublicationsLoading } =
+    useExplorePublicationsQuery({
+      request: {
+        sortCriteria: PublicationSortCriteria.TopCommented,
+        limit: PUB_LIMIT,
+      },
+    });
+  // get the list of profiles for the profileIds
+  const { data: profilesData, isLoading: isProfilesLoading } = useProfilesQuery(
+    {
+      request: {
+        profileIds,
+      },
+    },
+    {
+      // only run the query if there are profileIds
+      enabled: !!profileIds.length,
+    }
+  );
+
+  // get the array of publications
+  const publications = publicationsData?.explorePublications.items;
+
+  // get the array of profiles
+  const profiles = profilesData?.profiles.items;
+
+  // function to return the profileIds from the publications' profileIds
+  // add the CHINDU_GREY_PROFILE_ID to the set to avoid duplicates
+  const getProfileIds = () => {
+    if (!publications) {
+      return [];
+    }
+    // map the publications to get the profileIds
+    const Ids = publications.map((publication) => publication.profile.id);
+    // create a new set with the profileIds and the CHINDU_GREY_PROFILE_ID
+    const newProfileIds: Set<string> = new Set([
+      ...Ids,
+      CHINDU_GREY_PROFILE_ID,
+    ]);
+    // return the array of profileIds
+    return Array.from(newProfileIds);
+  };
 
   useEffect(() => {
-    if (!address) {
-      console.log("wallet not connected");
-      router.push("/connect");
+    if (!isPublicationsLoading && publications?.length === 5) {
+      setProfileIds(getProfileIds());
     }
-
-    // User not found
-    if (!user && address) {
-      console.log("User not found or not logged in");
-      router.push("/connect");
-    }
-  }, [address]);
-
-  if (address && user) {
-    return (
-      <>
-        {loading ? (
-          <LinkDotLoader callback={stopLoading} />
-        ) : (
-          <Main address={address} user={user}>
-            <Profile user={user} address={address} />
-          </Main>
-        )}
-      </>
-    );
-  }
-  return null;
+  }, [publications]);
+  return (
+    <>
+      {isProfilesLoading && <div>Loading...</div>}
+      {profiles?.map((profile) => (
+        <div key={profile.id}>
+          <div>{profile.handle}</div>
+        </div>
+      ))}
+      <ConnectComponent />
+    </>
+  );
 };
 
-export default Home;
+export default Explore;
